54694d4c4d41390c040d775550b9fc04b41f92e5
leafsoar <kltwjt@gmail.com>
2015-10-27 16:23:33 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,7 @@
+# Cocos 游戏资源更新
+
+## 说明
+
+使用 Golang + HTTP/1.1 + Json
+
+
---
+++ update.go
@@ -0,0 +1,12 @@
+//u
+// Author: leafsoar
+// Date: 2015-10-27 16:19:13
+//
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("leafsoar v5 ~")
+}
