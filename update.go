060042e0e3ab6c3484cb9c97203c742264b02710
leafsoar <kltwjt@gmail.com>
2015-10-27 16:57:33 +0800

    获取资源版本目录

--- update.go
+++ update.go
@@ -5,8 +5,37 @@
 
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	fp "path/filepath"
+	"strings"
+)
+
+// 计算每个文件的 MD5 值
 
 func main() {
-	fmt.Println("leafsoar v5 ~")
+	log.Println("leafsoar v5 ~")
+
+	// 首先获取 assets 目录下面的目录列表
+	list := GetSubDir("assets")
+	log.Println(list)
+}
+
+// GetSubDir 获取一个目录的子目录，第一级
+func GetSubDir(root string) []string {
+	var slice []string
+	fp.Walk(root, func(path string, f os.FileInfo, err error) error {
+		val := strings.Replace(path, root+"/", "", 1)
+		// 如果不是目录直接返回
+		if !f.IsDir() ||
+			strings.EqualFold(val, root) ||
+			!strings.EqualFold(val, f.Name()) {
+			return nil
+		}
+		// log.Println(path)
+		slice = append(slice, path)
+		return nil
+	})
+	return slice
 }
