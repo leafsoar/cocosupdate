11e2084781e116be0a3501bc9d9746aed14a2375
leafsoar <kltwjt@gmail.com>
2015-10-27 18:30:31 +0800

    添加 JSON 库使用

--- manifest.go
+++ manifest.go
@@ -7,9 +7,29 @@
 
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/bitly/go-simplejson"
+)
 
 // TestJSON 测试 json
 func TestJSON() {
 	log.Println("leafsoar")
+
+	js := simplejson.New()
+	js.Set("packageUrl", "http://192.168.1.50/res")
+	js.Set("remoteManifestUrl", "http://192.168.1.50/res")
+	js.Set("remoteVersionUrl", "http://192.168.1.50/res")
+	js.Set("version", "1.0.0")
+
+	gv := simplejson.New()
+	gv.Set("1", "1.0.1")
+	gv.Set("2", "1.0.2")
+	js.Set("groupVersions", gv)
+
+	// at := simplejson.New()
+
+	con, _ := js.EncodePretty()
+	log.Println(string(con))
 }
