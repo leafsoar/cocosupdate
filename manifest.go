004a88161e9554b3919edd399128c71443a70a4c
leafsoar <kltwjt@gmail.com>
2015-10-28 10:29:51 +0800

    定义 Project 类型

--- manifest.go
+++ manifest.go
@@ -8,28 +8,62 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
-
-	"github.com/bitly/go-simplejson"
 )
+
+// Manifest 资源描述文件
+type Manifest struct {
+	PackageURL        string            `json:"packageUrl"`
+	RemoteManifestURL string            `json:"remoteManifestUrl"`
+	RemoteVersionURL  string            `json:"remoteVersionUrl"`
+	Version           string            `json:"version"`
+	GroupVersions     map[string]string `json:"groupVersions"`
+	EngineVersion     string            `json:"engineVersion"`
+	Assets            map[string]Asset  `json:"assets"`
+	SearchPaths       []string          `json:"searchPaths"`
+}
+
+// SetURL 设置路径
+func (m *Manifest) SetURL(url string) {
+	m.PackageURL = url
+	m.RemoteManifestURL = url + "/project.manifest"
+	m.RemoteVersionURL = url + "/version.manifest"
+}
+
+// Asset 资源
+type Asset struct {
+	Path       string `json:"path"`
+	MD5        string `json:"md5"`
+	Compressed bool   `json:"compressed"`
+	Group      string `json:"group"`
+}
 
 // TestJSON 测试 json
 func TestJSON() {
 	log.Println("leafsoar")
 
-	js := simplejson.New()
-	js.Set("packageUrl", "http://192.168.1.50/res")
-	js.Set("remoteManifestUrl", "http://192.168.1.50/res")
-	js.Set("remoteVersionUrl", "http://192.168.1.50/res")
-	js.Set("version", "1.0.0")
+	mf := Manifest{}
+	mf.SetURL("http://192.168.1.50/res")
+	mf.Version = "1.0.0"
+	mf.GroupVersions = map[string]string{
+		"1": "1.0.1",
+		"2": "1.0.1",
+	}
+	mf.EngineVersion = "3.7.1"
+	mf.SearchPaths = []string{}
 
-	gv := simplejson.New()
-	gv.Set("1", "1.0.1")
-	gv.Set("2", "1.0.2")
-	js.Set("groupVersions", gv)
+	// assets
+	assets := map[string]Asset{}
+	assets["update1"] = Asset{
+		Path:       "src/app.zip",
+		MD5:        "lskjdklfjlsjdfl",
+		Compressed: true,
+		Group:      "1",
+	}
 
-	// at := simplejson.New()
+	mf.Assets = assets
 
-	con, _ := js.EncodePretty()
+	con, _ := json.MarshalIndent(mf, "", "  ")
 	log.Println(string(con))
 }
