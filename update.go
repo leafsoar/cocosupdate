4fd48aa8f26de5c7ad2d0d7346907a2f80bf1027
leafsoar <kltwjt@gmail.com>
2015-11-21 15:20:35 +0800

    HTTP 服务器参数化

--- update.go
+++ update.go
@@ -18,22 +18,22 @@ import (
 )
 
 // 根据源资源，生成发布资源
-func publish(url string, assets string, publish string, name string) {
-	fmt.Println("发布地址: ", url)
+func publish(address string, assets string, publish string, name string) {
 	fmt.Println("资源目录: ", assets)
 	fmt.Println("发布目录: ", publish)
+	fmt.Println("发布地址: ", address)
 	fmt.Println("发布名称: ", name)
 	ch := channel.NewChannel(name, assets, publish)
 	ch.InitVersions()
 	// 发布
-	ch.Publish(url)
-	fmt.Printf("发布完成: http://%s/%s/%s", url, publish, name)
+	ch.Publish("http://" + address)
+	fmt.Printf("发布完成: http://%s/%s\n", address, name)
 }
 
-func startServer(port string) {
-	log.Printf("启动 Http 服务 %s ...", port)
+func startServer(port string, publish string) {
+	log.Printf("启动 Http 服务 path: %s port: %s...", publish, port)
 
-	http.Handle("/", http.FileServer(http.Dir("publish")))
+	http.Handle("/", http.FileServer(http.Dir(publish)))
 	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		panic("Error: " + err.Error())
@@ -47,11 +47,9 @@ func handleSignals() {
 }
 
 func main() {
-	// fmt.Println("Cocos 热更新")
-
 	app := cli.NewApp()
 	app.Name = "Cocos Update"
-	app.Usage = "Cocos 资源热更新部署工具 (配合 AssetsManagerEx 使用)"
+	app.Usage = "Cocos 资源热更新部署工具 (请配合 AssetsManagerEx 使用)"
 	app.Version = "0.3.3"
 	app.Author = "leafsoar"
 	app.Email = "kltwjt@gmail.com"
@@ -59,26 +57,26 @@ func main() {
 	app.Commands = []cli.Command{
 		{
 			Name:        "build",
-			Usage:       "构建更新资源文件",
+			Usage:       "构建热更新资源文件",
 			Description: "默认发布地址: http://localhost:8000/res",
 			Flags: []cli.Flag{
 				cli.StringFlag{
 					Name:  "address, addr",
 					Value: "localhost:8000",
-					Usage: "发布资源地址 (host:port)",
+					Usage: "发布资源地址 (ip:port)",
 				},
 				cli.StringFlag{
-					Name:  "assets",
+					Name:  "assets, a",
 					Value: "assets",
 					Usage: "资源目录 (包含各个版本号的资源)",
 				},
 				cli.StringFlag{
-					Name:  "publish",
+					Name:  "publish, p",
 					Value: "publish",
 					Usage: "发布目录 (发布资源根目录)",
 				},
 				cli.StringFlag{
-					Name:  "name",
+					Name:  "name, n",
 					Value: "res",
 					Usage: "发布名称 (区分渠道或者地址)",
 				},
@@ -97,20 +95,23 @@ func main() {
 		{
 			Name:  "start",
 			Usage: "启动 HTTP 资源服务器",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "port, p",
+					Value: "8000",
+					Usage: "热更新端口号",
+				},
+				cli.StringFlag{
+					Name:  "publish, pub",
+					Value: "publish",
+					Usage: "热更新资源目录",
+				},
+			},
 			Action: func(c *cli.Context) {
-				fmt.Println("HTTP 服务器已经开启 ...")
+				startServer(c.String("port"), c.String("publish"))
+				handleSignals()
 			},
 		},
 	}
-
 	app.Run(os.Args)
-
-	// port := "8001"
-	// url := "http://192.168.1.51:" + port
-	// // 发布资源
-	// publish(url)
-	// // 启动服务
-	// startServer(port)
-	// // 监听 kill
-	// handleSignals()
 }
